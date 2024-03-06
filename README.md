e8225aee134472226d019fa849797fe20e0e8528
Tech0ne <clement.piasco@epitech.eu>
2024-03-07 00:49:13 +0100

    [~] Added self clean feature (using GNU attributes)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Library-C-GarbageCollector
 Sample C Garbage Collector
+
+---
+
+If you want the fg_cleanup function to be automaticly executed at the end of your program, write
+```c
+#define FGC_SELF_CLEAN
+```
+**BEFORE** including `f_garbage_collector.h`.
+
+If you are not using GNU, you will get an error.
--- includes/f_garbage_collector.h
+++ includes/f_garbage_collector.h
@@ -17,7 +17,14 @@ typedef struct _garbage_list {
 } _garbage_list;
 typedef _garbage_list* garbage_list;
 
+
+#if (defined(__GNUC__) && defined(FGC_SELF_CLEAN))
+__attribute__((destructor)) void fgc_cleanup(void);
+#elif (defined(FGC_SELF_CLEAN))
+    #error "Can not use self clean features while not in GNU !"
+#else
 void fgc_cleanup(void);
+#endif
 
 void *fgc_malloc(size_t size);
 void fgc_free(void *ptr);
